854eee5a4e8d3b49674dc21276a4e70753dccd08
wjaszczak <jaszczakw@gmail.com>
2014-09-19 14:24:03 +0200

    parallaxes fix

--- js/script.js
+++ js/script.js
@@ -1,18 +1,24 @@
 $(function (){
-	var firstFoldHeight = $('#first-fold').height();
 
 	$(window).scroll(function(e) {
       var _wheight = $(window).height();
-      
+	  //var firstFoldHeight = $('#first-fold').height();
+	  //var secondFoldHeight = $('#second-fold').height();
+
       // Home
       var _scroll_top = $(window).scrollTop();
       var _margin_top = _scroll_top/2;
-      //$('.parallax-divider.black').css('margin-top', -_margin_top);
-      $('#first-fold').css('height', firstFoldHeight -_margin_top);
+
+      $('#first-fold').css('height', 807 -_margin_top);
+      
+
+
+
+      // $('#second-fold').css('height', secondFoldHeight + 10 -_margin_top);
 
 
       if(_margin_top>150){
-      	$('.parallax-divider.black').css('height', 156-_margin_top+150);
+          $('.parallax-divider.black').css('height', 156-_margin_top+150);
 	  }
-    }).scroll();
+    });
 });
