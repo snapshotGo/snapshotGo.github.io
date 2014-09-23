571773a79d97f6186473bc0635fe18af726945dd
wjaszczak <jaszczakw@gmail.com>
2014-09-23 13:48:28 +0200

    Optimized website

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,3 +1,63 @@
+$(function(){
+    //Preparations for parallax
+
+    $('#first-fold').css({
+        'top': 0,
+        'position': 'fixed',
+        'width': '100%'
+    });
+
+    $('#first-fold').closest('section').css('height','100%');
+
+    $('.parallax-divider.pink').css({
+        transform: 'translateY(0px)',
+        'z-index': 10,
+        position: 'absolute',
+        width: '100%'
+    });
+
+
+    $('.parallax-divider.black').css({
+        transform: 'translateY(0px)',
+        'z-index': 10,
+        position: 'absolute',
+        width: '100%',
+        'margin-bottom': '50px'
+    });
+
+    $('#how-it-works').css('margin-top',49);
+});
+
+$(window).scroll(function(e) {
+
+    var _wheight = $(window).height();
+
+    var _scroll_top = $(window).scrollTop();
+    var _margin_top = _scroll_top / 2;
+
+    //First fold move 2 times slower
+    $('#first-fold').css('margin-top', -_margin_top);
+
+    var pinkTop = $('.parallax-divider.pink').offset().top;
+
+
+    //dividers move faster
+    if($('#sign-up').offset().top <= window.screen.availHeight + window.scrollY){
+        var initialScroll = $('#sign-up').offset().top - window.innerHeight ;
+        var dividerHeight = $('.parallax-divider.pink').outerHeight();
+        var toScroll = Math.max(-((window.scrollY-initialScroll)/2),-dividerHeight);
+        $('.parallax-divider.pink').css('margin-top',toScroll);
+    }
+
+
+    if($('#how-it-works').offset().top <= window.screen.availHeight + window.scrollY + 125){
+        var initialScroll = $('#how-it-works').offset().top - window.innerHeight + 125;
+        var dividerHeight = $('.parallax-divider.pink').outerHeight();
+        var toScroll = Math.max(-((window.scrollY-initialScroll)/2),-dividerHeight+50);
+        $('.parallax-divider.black').css('margin-top',toScroll);
+    }
+});
+
 $(function() {
 
     function contains(text, searchText) {
@@ -25,7 +85,7 @@ $(function() {
         }
 
 
-        if ($.address.history()) {
+        // if ($.address.history()) {
             _scroll_animation_running = true;
             _set_active_nav_item(_target_page_id);
 
@@ -43,7 +103,7 @@ $(function() {
                     _scroll_animation_running = false;
                 }
             });
-        }
+        // }
 
     });
 
--- scripts/thirdParty.js
+++ scripts/thirdParty.js
@@ -610,3 +610,301 @@
     };
     
 })(jQuery);
+/**
+ * Copyright (c) 2007-2013 Ariel Flesler - aflesler<a>gmail<d>com | http://flesler.blogspot.com
+ * Dual licensed under MIT and GPL.
+ * @author Ariel Flesler
+ * @version 1.4.6
+ */
+;(function($){var h=$.scrollTo=function(a,b,c){$(window).scrollTo(a,b,c)};h.defaults={axis:'xy',duration:parseFloat($.fn.jquery)>=1.3?0:1,limit:true};h.window=function(a){return $(window)._scrollable()};$.fn._scrollable=function(){return this.map(function(){var a=this,isWin=!a.nodeName||$.inArray(a.nodeName.toLowerCase(),['iframe','#document','html','body'])!=-1;if(!isWin)return a;var b=(a.contentWindow||a).document||a.ownerDocument||a;return/webkit/i.test(navigator.userAgent)||b.compatMode=='BackCompat'?b.body:b.documentElement})};$.fn.scrollTo=function(e,f,g){if(typeof f=='object'){g=f;f=0}if(typeof g=='function')g={onAfter:g};if(e=='max')e=9e9;g=$.extend({},h.defaults,g);f=f||g.duration;g.queue=g.queue&&g.axis.length>1;if(g.queue)f/=2;g.offset=both(g.offset);g.over=both(g.over);return this._scrollable().each(function(){if(e==null)return;var d=this,$elem=$(d),targ=e,toff,attr={},win=$elem.is('html,body');switch(typeof targ){case'number':case'string':if(/^([+-]=?)?\d+(\.\d+)?(px|%)?$/.test(targ)){targ=both(targ);break}targ=$(targ,this);if(!targ.length)return;case'object':if(targ.is||targ.style)toff=(targ=$(targ)).offset()}$.each(g.axis.split(''),function(i,a){var b=a=='x'?'Left':'Top',pos=b.toLowerCase(),key='scroll'+b,old=d[key],max=h.max(d,a);if(toff){attr[key]=toff[pos]+(win?0:old-$elem.offset()[pos]);if(g.margin){attr[key]-=parseInt(targ.css('margin'+b))||0;attr[key]-=parseInt(targ.css('border'+b+'Width'))||0}attr[key]+=g.offset[pos]||0;if(g.over[pos])attr[key]+=targ[a=='x'?'width':'height']()*g.over[pos]}else{var c=targ[pos];attr[key]=c.slice&&c.slice(-1)=='%'?parseFloat(c)/100*max:c}if(g.limit&&/^\d+$/.test(attr[key]))attr[key]=attr[key]<=0?0:Math.min(attr[key],max);if(!i&&g.queue){if(old!=attr[key])animate(g.onAfterFirst);delete attr[key]}});animate(g.onAfter);function animate(a){$elem.animate(attr,f,g.easing,a&&function(){a.call(this,targ,g)})}}).end()};h.max=function(a,b){var c=b=='x'?'Width':'Height',scroll='scroll'+c;if(!$(a).is('html,body'))return a[scroll]-$(a)[c.toLowerCase()]();var d='client'+c,html=a.ownerDocument.documentElement,body=a.ownerDocument.body;return Math.max(html[scroll],body[scroll])-Math.min(html[d],body[d])};function both(a){return typeof a=='object'?a:{top:a,left:a}}})(jQuery);
+
+
+
+/*!
+ * jQuery Viewport Plugin
+ *
+ * Date: Jul 29 2011
+ *
+ * Copyright 2011, Koen Punt / http://www.koenpunt.nl
+ * Dual licensed under the MIT and GPL licenses (just like jQuery):
+ * http://www.opensource.org/licenses/mit-license.php
+ * http://www.gnu.org/licenses/gpl.html
+ *
+*/
+
+;(function( $ ){
+	$.fn.extend({
+		
+		inViewport: function(){
+			var visibleElements = [];
+			this.each(function(){
+				if($.inViewport(this)){
+					visibleElements.push(this);
+				}	
+			});
+			return this.pushStack(visibleElements);
+		},
+		
+		mostVisible: function(){
+			var visibleElements = this.inViewport(),
+				mostVisible = null,
+				highestVisibility = 0;
+			
+			$.each(visibleElements, function(index, element){
+				if(elementVisibility = $(element).data('vpVisibility')){
+					if(elementVisibility > highestVisibility){
+						highestVisibility = elementVisibility;
+						mostVisible = $(element);
+					}
+				}
+			});
+		
+			return mostVisible;
+		}
+	});
+	
+	$.inViewport = function(element){
+		var element = $(element),
+			scrollTop = $(window).scrollTop(),
+			windowHeight = $(window).height(),
+			offset = element.offset(),
+			elementBottom = offset.top + element.height() - scrollTop,// - 100,
+			elementTop = offset.top - scrollTop,// - 100,
+			a,b;
+			a = elementTop - (windowHeight - element.height()) > element.height() ? element.height() : elementTop - (windowHeight - element.height());
+			a = a > 0 ? a : 0;
+			b = elementBottom > element.height() ? element.height() : elementBottom;
+			b = b > 0 ? b : 0;
+			vpVisibility = (b-a) / element.height();
+			element.data('vpVisibility', vpVisibility);
+		return vpVisibility;
+	};
+	
+})( jQuery );
+
+
+
+/*
+ * jQuery Easing v1.3 - http://gsgd.co.uk/sandbox/jquery/easing/
+ *
+*/
+jQuery.easing.jswing=jQuery.easing.swing;jQuery.extend(jQuery.easing,{def:"easeOutQuad",swing:function(e,f,a,h,g){return jQuery.easing[jQuery.easing.def](e,f,a,h,g)},easeInQuad:function(e,f,a,h,g){return h*(f/=g)*f+a},easeOutQuad:function(e,f,a,h,g){return -h*(f/=g)*(f-2)+a},easeInOutQuad:function(e,f,a,h,g){if((f/=g/2)<1){return h/2*f*f+a}return -h/2*((--f)*(f-2)-1)+a},easeInCubic:function(e,f,a,h,g){return h*(f/=g)*f*f+a},easeOutCubic:function(e,f,a,h,g){return h*((f=f/g-1)*f*f+1)+a},easeInOutCubic:function(e,f,a,h,g){if((f/=g/2)<1){return h/2*f*f*f+a}return h/2*((f-=2)*f*f+2)+a},easeInQuart:function(e,f,a,h,g){return h*(f/=g)*f*f*f+a},easeOutQuart:function(e,f,a,h,g){return -h*((f=f/g-1)*f*f*f-1)+a},easeInOutQuart:function(e,f,a,h,g){if((f/=g/2)<1){return h/2*f*f*f*f+a}return -h/2*((f-=2)*f*f*f-2)+a},easeInQuint:function(e,f,a,h,g){return h*(f/=g)*f*f*f*f+a},easeOutQuint:function(e,f,a,h,g){return h*((f=f/g-1)*f*f*f*f+1)+a},easeInOutQuint:function(e,f,a,h,g){if((f/=g/2)<1){return h/2*f*f*f*f*f+a}return h/2*((f-=2)*f*f*f*f+2)+a},easeInSine:function(e,f,a,h,g){return -h*Math.cos(f/g*(Math.PI/2))+h+a},easeOutSine:function(e,f,a,h,g){return h*Math.sin(f/g*(Math.PI/2))+a},easeInOutSine:function(e,f,a,h,g){return -h/2*(Math.cos(Math.PI*f/g)-1)+a},easeInExpo:function(e,f,a,h,g){return(f==0)?a:h*Math.pow(2,10*(f/g-1))+a},easeOutExpo:function(e,f,a,h,g){return(f==g)?a+h:h*(-Math.pow(2,-10*f/g)+1)+a},easeInOutExpo:function(e,f,a,h,g){if(f==0){return a}if(f==g){return a+h}if((f/=g/2)<1){return h/2*Math.pow(2,10*(f-1))+a}return h/2*(-Math.pow(2,-10*--f)+2)+a},easeInCirc:function(e,f,a,h,g){return -h*(Math.sqrt(1-(f/=g)*f)-1)+a},easeOutCirc:function(e,f,a,h,g){return h*Math.sqrt(1-(f=f/g-1)*f)+a},easeInOutCirc:function(e,f,a,h,g){if((f/=g/2)<1){return -h/2*(Math.sqrt(1-f*f)-1)+a}return h/2*(Math.sqrt(1-(f-=2)*f)+1)+a},easeInElastic:function(f,h,e,l,k){var i=1.70158;var j=0;var g=l;if(h==0){return e}if((h/=k)==1){return e+l}if(!j){j=k*0.3}if(g<Math.abs(l)){g=l;var i=j/4}else{var i=j/(2*Math.PI)*Math.asin(l/g)}return -(g*Math.pow(2,10*(h-=1))*Math.sin((h*k-i)*(2*Math.PI)/j))+e},easeOutElastic:function(f,h,e,l,k){var i=1.70158;var j=0;var g=l;if(h==0){return e}if((h/=k)==1){return e+l}if(!j){j=k*0.3}if(g<Math.abs(l)){g=l;var i=j/4}else{var i=j/(2*Math.PI)*Math.asin(l/g)}return g*Math.pow(2,-10*h)*Math.sin((h*k-i)*(2*Math.PI)/j)+l+e},easeInOutElastic:function(f,h,e,l,k){var i=1.70158;var j=0;var g=l;if(h==0){return e}if((h/=k/2)==2){return e+l}if(!j){j=k*(0.3*1.5)}if(g<Math.abs(l)){g=l;var i=j/4}else{var i=j/(2*Math.PI)*Math.asin(l/g)}if(h<1){return -0.5*(g*Math.pow(2,10*(h-=1))*Math.sin((h*k-i)*(2*Math.PI)/j))+e}return g*Math.pow(2,-10*(h-=1))*Math.sin((h*k-i)*(2*Math.PI)/j)*0.5+l+e},easeInBack:function(e,f,a,i,h,g){if(g==undefined){g=1.70158}return i*(f/=h)*f*((g+1)*f-g)+a},easeOutBack:function(e,f,a,i,h,g){if(g==undefined){g=1.70158}return i*((f=f/h-1)*f*((g+1)*f+g)+1)+a},easeInOutBack:function(e,f,a,i,h,g){if(g==undefined){g=1.70158}if((f/=h/2)<1){return i/2*(f*f*(((g*=(1.525))+1)*f-g))+a}return i/2*((f-=2)*f*(((g*=(1.525))+1)*f+g)+2)+a},easeInBounce:function(e,f,a,h,g){return h-jQuery.easing.easeOutBounce(e,g-f,0,h,g)+a},easeOutBounce:function(e,f,a,h,g){if((f/=g)<(1/2.75)){return h*(7.5625*f*f)+a}else{if(f<(2/2.75)){return h*(7.5625*(f-=(1.5/2.75))*f+0.75)+a}else{if(f<(2.5/2.75)){return h*(7.5625*(f-=(2.25/2.75))*f+0.9375)+a}else{return h*(7.5625*(f-=(2.625/2.75))*f+0.984375)+a}}}},easeInOutBounce:function(e,f,a,h,g){if(f<g/2){return jQuery.easing.easeInBounce(e,f*2,0,h,g)*0.5+a}return jQuery.easing.easeOutBounce(e,f*2-g,0,h,g)*0.5+h*0.5+a}});
+
+
+
+/**
+ * Within Viewport
+ *
+ * @description Determines whether an element is completely
+ *              within the browser viewport
+ * @author      Craig Patik, http://patik.com/
+ * @version     0.0.4
+ * @date        2014-07-05
+ */
+;(function() {
+    /**
+     * Determines whether an element is within the viewport
+     * @param  {Object}  elem       DOM Element (required)
+     * @param  {Object}  options    Optional settings
+     * @return {Boolean}            Whether the element was completely within the viewport
+    */
+    var withinViewport = function _withinViewport(elem, options) {
+        var result = false,
+            metadata = {},
+            config = {},
+            settings, useHtmlElem, isWithin, scrollOffset, elemOffset, arr, i, side;
+
+        if (typeof jQuery !== 'undefined' && elem instanceof jQuery) {
+            elem = elem.get(0);
+        }
+
+        if (typeof elem !== 'object' || elem.nodeType !== 1) {
+            throw new Error('First argument must be an element');
+        }
+
+        if (elem.getAttribute('data-withinviewport-settings') && window.JSON) {
+            metadata = JSON.parse(elem.getAttribute('data-withinviewport-settings'));
+        }
+
+        // Settings argument may be a simple string (`top`, `right`, etc)
+        if (typeof options === 'string') {
+            settings = {sides: options};
+        }
+        else {
+            settings = options || {};
+        }
+
+        // Build configuration from defaults and given settings
+        config.container = settings.container || metadata.container || withinViewport.defaults.container || document.body;
+        config.sides = settings.sides || metadata.sides || withinViewport.defaults.sides || 'all';
+        config.top = settings.top || metadata.top || withinViewport.defaults.top || 0;
+        config.right = settings.right || metadata.right || withinViewport.defaults.right || 0;
+        config.bottom = settings.bottom || metadata.bottom || withinViewport.defaults.bottom || 0;
+        config.left = settings.left || metadata.left || withinViewport.defaults.left || 0;
+
+        // Whether we can use the `<html`> element for `scrollTop`
+        // Unfortunately at the moment I can't find a way to do this without UA-sniffing
+        useHtmlElem = !/Chrome/.test(navigator.userAgent);
+
+        // Element testing methods
+        isWithin = {
+            // Element is below the top edge of the viewport
+            top: function _isWithin_top() {
+                return elemOffset[1] >= scrollOffset[1] + config.top;
+            },
+
+            // Element is to the left of the right edge of the viewport
+            right: function _isWithin_right() {
+                var container = (config.container === document.body) ? window : config.container;
+
+                return elemOffset[0] + elem.offsetWidth <= container.innerWidth + scrollOffset[0] - config.right;
+            },
+
+            // Element is above the bottom edge of the viewport
+            bottom: function _isWithin_bottom() {
+                var container = (config.container === document.body) ? window : config.container;
+
+                return elemOffset[1] + elem.offsetHeight <= scrollOffset[1] + container.innerHeight - config.bottom;
+            },
+
+            // Element is to the right of the left edge of the viewport
+            left: function _isWithin_left() {
+                return elemOffset[0] >= scrollOffset[0] + config.left;
+            },
+
+            all: function _isWithin_all() {
+                return (isWithin.top() && isWithin.right() && isWithin.bottom() && isWithin.left());
+            }
+        };
+
+        // Current offset values
+        scrollOffset = (function _scrollOffset() {
+            var x = config.container.scrollLeft,
+                y = config.container.scrollTop;
+
+            if (y === 0) {
+                if (config.container.pageYOffset) {
+                    y = config.container.pageYOffset;
+                }
+                else if (window.pageYOffset) {
+                    y = window.pageYOffset;
+                }
+                else {
+                    if (config.container === document.body) {
+                        if (useHtmlElem) {
+                            y = (config.container.parentElement) ? config.container.parentElement.scrollTop : 0;
+                        }
+                        else {
+                            y = (config.container.parentElement) ? config.container.parentElement.scrollTop : 0;
+                        }
+                    }
+                    else {
+                        y = (config.container.parentElement) ? config.container.parentElement.scrollTop : 0;
+                    }
+                }
+            }
+
+            if (x === 0) {
+                if (config.container.pageXOffset) {
+                    x = config.container.pageXOffset;
+                }
+                else if (window.pageXOffset) {
+                    x = window.pageXOffset;
+                }
+                else {
+                    if (config.container === document.body) {
+                        x = (config.container.parentElement) ? config.container.parentElement.scrollLeft : 0;
+                    }
+                    else {
+                        x = (config.container.parentElement) ? config.container.parentElement.scrollLeft : 0;
+                    }
+                }
+            }
+
+            return [x, y];
+        }());
+
+        elemOffset = (function _elemOffset() {
+            var el = elem,
+                x = 0,
+                y = 0;
+
+            if (el.parentNode) {
+                x = el.offsetLeft;
+                y = el.offsetTop;
+
+                el = el.parentNode;
+                while (el) {
+                    if (el == config.container) {
+                        break;
+                    }
+
+                    x += el.offsetLeft;
+                    y += el.offsetTop;
+
+                    el = el.parentNode;
+                }
+            }
+
+            return [x, y];
+        })();
+
+        // Test the element against each side of the viewport that was requested
+        arr = config.sides.split(' ');
+        i = arr.length;
+        while (i--) {
+            side = arr[i].toLowerCase();
+            if (/top|right|bottom|left|all/.test(side)) {
+                if (isWithin[side]()) {
+                    result = true;
+                }
+                else {
+                    result = false;
+                    // Quit as soon as the first failure is found
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }; // end of `withinViewport()`
+
+    // Default settings
+    withinViewport.prototype.defaults = {
+        container: document.body,
+        sides: 'all',
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0
+    };
+
+    withinViewport.defaults = withinViewport.prototype.defaults;
+
+    // Make function available globally
+    window.withinViewport = withinViewport;
+
+    /**
+     * Optional enhancements and shortcuts
+     *
+     * @description Uncomment or comment these pieces as they apply to your project and coding preferences
+     */
+
+    // Shortcut methods for each side of the viewport
+    // Ex: withinViewport.top(elem) is the same as withinViewport(elem, 'top')
+    withinViewport.prototype.top = function _withinViewport_top(element) {
+        return withinViewport(element, 'top');
+    };
+
+    withinViewport.prototype.right = function _withinViewport_right(element) {
+        return withinViewport(element, 'right');
+    };
+
+    withinViewport.prototype.bottom = function _withinViewport_bottom(element) {
+        return withinViewport(element, 'bottom');
+    };
+
+    withinViewport.prototype.left = function _withinViewport_left(element) {
+        return withinViewport(element, 'left');
+    };
+}());
