0485bb558367690b5c2e18553af1de5b966f647b
wjaszczak <jaszczakw@gmail.com>
2014-09-23 11:09:48 +0200

    Pink parallax.

--- js/parallax.js
+++ js/parallax.js
@@ -17,7 +17,41 @@ $(window).scroll(function(e) {
 
 	$('#first-fold').closest('section').css('height','100%');
 
-    // //$('#first-fold').find()
+    // $('#sign-up').css('background','linear-gradient(to right, rgba(254,254,254,0.1) 0%,rgba(0,0,0,0.25)'+ _margin_top/10 + '%)');
+
+    var pinkTop = $('.parallax-divider.pink').offset().top;
+
+
+    $('.parallax-divider.pink').css({
+        transform: 'translateY(0px)',
+        'z-index': 10,
+        position: 'absolute',
+        width: '100%'
+    });
+
+    if($('#sign-up').offset().top <= window.screen.availHeight + window.scrollY){
+        var initialScroll = $('#sign-up').offset().top - window.innerHeight ;
+        var dividerHeight = $('.parallax-divider.pink').outerHeight();
+        var toScroll = Math.max(-((window.scrollY-initialScroll)/2),-dividerHeight);
+        $('.parallax-divider.pink').css('margin-top',toScroll);
+    }
+
+
+    // $('.parallax-divider.black').css({
+    //     transform: 'translateY(0px)',
+    //     'z-index': 10,
+    //     position: 'absolute',
+    //     width: '100%',
+    //     // 'margin-bottom': '50px'
+    // });
+
+    // if($('#how-it-works').offset().top <= window.screen.availHeight + window.scrollY){
+    //     var initialScroll = $('#how-it-works').offset().top - window.innerHeight;
+    //     var dividerHeight = $('.parallax-divider.pink').outerHeight();
+    //     var toScroll = Math.max(-((window.scrollY-initialScroll)/2),-dividerHeight+50);
+    //     $('.parallax-divider.black').css('margin-top',toScroll);
+    // }
+
 
     // if (_margin_top > 150) {
     // $('.parallax-divider.black').css('height', 156 - _margin_top + 150);
